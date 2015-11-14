92866b4db29af6f20030ba091426dffc4d9cf358
talpallikar <arjuntalpallikar@utexas.edu>
2015-11-14 07:57:19 -0500

    added a resourcce page and worked on splitting js and html

---
+++ Test/image change.js
@@ -0,0 +1,8 @@
+imagechange.js
+function changeImage() {
+    var image = document.getElementById('myImage');
+    if (image.src.match("TrumpGreen")) {
+        image.src = "Trumpdefault.jpg";
+    } else {
+        image.src = "TrumpGreen.jpg";
+    }
