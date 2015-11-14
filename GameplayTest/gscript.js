6686a130b6ba9028d8017a837b28e20eb39e287a
talpallikar <arjuntalpallikar@utexas.edu>
2015-11-14 15:36:29 -0500

    FINAL FUNCTIONALITY COMPLETED FOR SINGLE QUESTION USE CASE
    :)

--- GameplayTest/gscript.js
+++ GameplayTest/gscript.js
@@ -1,17 +1,15 @@
-window.QuestionPlaying = false;		//global variable that determines if a question is running
+var QuestionPlaying = new Boolean(false);
+		//global variable that determines if a question is running
 window.RealAnswer;						//global variable that stores the correct answer for the current question
 window.SelectedAnswer;					//global variable that stores the player's answer
 window.HappyMessage = "Good Job!"		//global variable containing response if player is correct
 window.SadMessage	= "You're Fired!"	//global variable containing response if player is wrong
 
-function DisplayQuote() {			//function that displays new questions
-if (QuestionPlaying = true) {
-	document.getElementById("quote").innerHTML = "FAILURE :(";	//Displays a quote
 
-}
-else
+
+function DisplayQuote() {			//function that displays new questions
 {
-//QuestionPlaying = true;				//Starts a question cycle
+QuestionPlaying = true;				//Starts a question cycle
 var ToBeDisplayed;					//Variable that stores a number that will determine which quote is displayed
 var QuoteDB;						//Variable that holds the correct database of questions
 
@@ -83,27 +81,69 @@ document.getElementById("quote").innerHTML = QuoteDB[ToBeDisplayed];	//Displays 
 
 function SelectTrump()		//Initializes answering process if Trump is the selected answer choice
 {
-	if (QuestionPlaying = true) 
+	if (QuestionPlaying === true) 
 	{
-		changeImage();
-		//document.getElementById('trumptest').innerHTML = "SUCCESS!!!!"
-		//SelectedAnswer = "Trump";
-		//answerQuote();
+		SelectedAnswer = "Trump";
+		answerQuote();
 	}
-	
-	//else 
-	//{
-	//	changeImage2();
-	//}
 }
 
 function SelectColbert()	//Initializes answering process if Colbert is the selected Answer Choice
 {
-	if (QuestionPlaying = true) 
+	if (QuestionPlaying === true) 
 	{
-		changeImage2();
-		//document.getElementById('colbtest').innerHTML = "SUCCESS!!!!"
-		//SelectedAnswer = "Colbert";
-		//answerQuote();
+		SelectedAnswer = "Colbert";
+		answerQuote();
 	}
+}
+
+function answerQuote()		//Checks if the player is correct or incorrect
+{
+	if (SelectedAnswer === RealAnswer)
+		{
+			
+     	 	CorrectAnswer();
+		}
+	
+	else
+		{
+			WrongAnswer();
+		}
+}
+
+function CorrectAnswer()	//Color functionality if player is correct
+{
+	if 	(SelectedAnswer === "Colbert") 
+		{		
+				var image = document.getElementById('myImage');
+		    	var image2 = document.getElementById('myImage2');
+				image.src = "../Images/Trumpfade.jpg";
+		        image2.src = "../Images/colbertgreen.jpg";
+		}
+
+	else
+		{
+				var image = document.getElementById('myImage');
+		    	var image2 = document.getElementById('myImage2');
+				image.src = "../Images/TrumpGreen.jpg";
+		        image2.src = "../Images/Colbertfade.jpg";
+		}
+
+	document.getElementById('whoQ').innerHTML = HappyMessage;
+}
+
+function WrongAnswer()		// Color functionality if player is wrong
+{
+	if (SelectedAnswer === "Colbert")
+		{
+			image.src = "../Images/NotSelectedTrumpWrong.jpg";
+		    image2.src = "../Images/SelectedColbertWrong.jpg";
+		}	
+
+	else
+		{
+			image.src = "../Images/SelectedTrumpWrong.jpg";
+		    image2.src = "../Images/NotSelectedColbertWrong.jpg";
+		}	
+	document.getElementById('whoQ').innerHTML = SadMessage;
 }
