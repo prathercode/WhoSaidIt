0e22469aedc0d5abb33fdf6f7f98b93a0894fe9a
talpallikar <arjuntalpallikar@utexas.edu>
2015-11-14 16:21:26 -0500

    i hate chris
    sorry for this not descriptive message this actually added the ayylmao
    lmao lmao ayy lmao ayylmao

--- Code/finalscript.js
+++ Code/finalscript.js
@@ -1,5 +1,5 @@
 var QuestionPlaying = new Boolean(false);
-		//global variable that determines if a question is running
+										//global variable that determines if a question is running
 window.RealAnswer;						//global variable that stores the correct answer for the current question
 window.SelectedAnswer;					//global variable that stores the player's answer
 window.HappyMessage = "Good Job!"		//global variable containing response if player is correct
@@ -7,38 +7,39 @@ window.SadMessage	= "You're Fired!"	//global variable containing response if pla
 
 
 
-function DisplayQuote() {			//function that displays new questions
-QuestionPlaying = true;				//Starts a question cycle
-var ToBeDisplayed;					//Variable that stores a number that will determine which quote is displayed
-var QuoteDB;						//Variable that holds the correct database of questions
+function DisplayQuote() 				//function that displays new questions
+{			
+	QuestionPlaying = true;				//Starts a question cycle
+	var ToBeDisplayed;					//Variable that stores a number that will determine which quote is displayed
+	var QuoteDB;						//Variable that holds the correct database of questions
 
-var ColbertQuotes = [				//Array Database of Colbert Quotes
-						"I apologize for being perfect", 
-					 	"Medicare is like a nice set of cufflinks. Nobody wears cufflinks anymore."
-				    ];
+	var ColbertQuotes = [				//Array Database of Colbert Quotes
+							"I apologize for being perfect", 
+					 		"Medicare is like a nice set of cufflinks. Nobody wears cufflinks anymore."
+				   		];
 
 
-var TrumpQuotes = [ 				//Array Database of Trump Quotes
-						"We got Obama in there now and the Chinese testing him bing bing bing. You get a woman in there bing bom boom the whole world goes after her.", 
-						"I think apologizing's a great thing, but you have to be wrong… I will absolutely apologize, sometimes in the hopefully distant future, if I'm ever wrong.",
-						"It's freezing and snowing in New York. We need global warming!", 
-						"So many people have told me that I should host Meet the Press and replace the moron who is on now. Just too busy, especially next 10 years!"
-				  ];
+	var TrumpQuotes = [ 				//Array Database of Trump Quotes
+							"We got Obama in there now and the Chinese testing him bing bing bing. You get a woman in there bing bom boom the whole world goes after her.", 
+							"I think apologizing's a great thing, but you have to be wrong… I will absolutely apologize, sometimes in the hopefully distant future, if I'm ever wrong.",
+							"It's freezing and snowing in New York. We need global warming!", 
+							"So many people have told me that I should host Meet the Press and replace the moron who is on now. Just too busy, especially next 10 years!"
+					  ];
 
-if(Math.random()<.5)				//Randomly determines if question should be a Colbert or Trump quote
-	{
-		QuoteDB = ColbertQuotes;
-		RealAnswer = "Colbert";
-	}
+	if(Math.random()<.5)				//Randomly determines if question should be a Colbert or Trump quote
+		{
+			QuoteDB = ColbertQuotes;
+			RealAnswer = "Colbert";
+		}
 
-else
-	{
-		QuoteDB = TrumpQuotes;
-		RealAnswer = "Trump";
-	}
+	else
+		{
+			QuoteDB = TrumpQuotes;
+			RealAnswer = "Trump";
+		}
 
-ToBeDisplayed = parseInt((Math.random() * QuoteDB.length), 10);
-document.getElementById("quote").innerHTML = QuoteDB[ToBeDisplayed];	//Displays a quote
+	ToBeDisplayed = parseInt((Math.random() * QuoteDB.length), 10);
+	document.getElementById("quote").innerHTML = QuoteDB[ToBeDisplayed];	//Displays a quote
 }
 
 
@@ -110,4 +111,13 @@ function WrongAnswer()		// Color functionality if player is wrong
 		    image2.src = "../Images/ColbertDefault.png";
 		}	
 	document.getElementById('whoQ').innerHTML = SadMessage;
+}
+
+function reset()
+{
+	var image = document.getElementById('myImage');
+	var image2 = document.getElementById('myImage2');
+		image.src = "../Images/Trumpdefault.jpg";
+		image2.src = "../Images/Colbertdefault.jpg";
+	document.getElementById('whoQ').innerHTML = 'WHO SAID THIS QUOTE?';
 }
